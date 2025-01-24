868516c95b8aea424dfb9483ee46f1b64957d358
Sera De La Rosa <sera.delarosa@outlook.com>
2025-01-24 01:08:07 -0500

    Exercise 5 done.

--- app.js
+++ app.js
@@ -65,3 +65,19 @@ console.log('Exercise 4 result:', favFood);
 
 
 // == Exercise 5 ===========================================================
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(2, 0, "tofu");
+
+console.log('Exercise 5 result:', foods);
+
+// == Exercise 6 =============================================================
+
+
